7a47fd3f053092422fd3d8cf90a8f25a19aaae77
Radika <radikakumar@gmail.com>
2021-06-05 17:00:12 +0100

    Developed new dictionary project

---
+++ dictionary-project/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+import Dictionary from "./Dictionary";
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Container">
+          </div>
+      <div className= "DictionaryApp">
+     📔 Dictionary App
+      </div>
+      <main>
+        <Dictionary />
+      </main>
+    
+      </header>
+    </div>
+  
+  );
+}
